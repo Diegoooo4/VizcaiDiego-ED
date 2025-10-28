3225ecc26690b537f8e5f13e2ef5222b6e1cc2dd
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-10-28 13:40:48 +0100

    Commit Resta

---
+++ basic0peration.java
@@ -0,0 +1,18 @@
+package ud2.exercises;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Basic0perations {
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in).useLocale(Locale.US);
+            System.out.print("Dime el primer número: ");
+            int a = in.nextInt();
+            System.out.print("Dime el segundo número: ");
+            int b = in.nextInt();
+
+            int resta = a - b;
+            System.out.print("Resta: "++resta);uma);
+    }
+}
+
+
