527e037da584cc4e55daab6f62aaae9993d20630
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-10-28 13:35:12 +0100

    Correcció Basic0perations

--- src/main/java/ud2/exercises/Basic0perations.java
+++ src/main/java/ud2/exercises/Basic0perations.java
@@ -5,8 +5,11 @@ import java.util.Locale;
 public class Basic0perations {
     public static void main(String[] args) {
             Scanner in = new Scanner(System.in).useLocale(Locale.US);
+            System.out.print("Dime el primer número: ");
             int a = in.nextInt();
+            System.out.print("Dime el segundo número: ");
             int b = in.nextInt();
+
     }
 }
 
