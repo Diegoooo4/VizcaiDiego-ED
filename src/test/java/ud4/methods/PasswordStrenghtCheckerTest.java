f9b738ff6e99a390cc4719a047e385ccdd2096cb
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-24 00:12:38 +0100

    Weak test

--- src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
@@ -1,3 +1,47 @@
 package ud4.methods;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
     class PasswordStrenghtCheckerTest {
+        @Nested
+        class PasswordStrengtWeak {
+
+
+            @Test
+            @DisplayName("Prueba de contraseña debil minuscula")
+            void WeakShouldReturnDebilMinuscula() {
+                //Arrange
+                String password ="abcdefgh";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.WEAK;
+                //Act
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+            @Test
+            @DisplayName("Prueba de contraseña debil mayusculas")
+            void WeakShouldReturnDebilMayuscula() {
+                //Act
+                String password ="ABCDEFGH";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.WEAK;
+                //Arange
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+            @Test
+            @DisplayName("Prueba de contraseña debil caracter")
+            void WeakShouldReturnDebilCaracter() {
+                //Act
+                String password ="!·$%&/()";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.WEAK;
+                //Arange
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+        }
     }
