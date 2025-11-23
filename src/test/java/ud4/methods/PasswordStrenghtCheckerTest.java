b18c1afc1b7369061f2a1a4999720ac12267c926
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-24 00:09:52 +0100

    test invalid

--- src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
@@ -1,3 +1,48 @@
 package ud4.methods;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
     class PasswordStrenghtCheckerTest {
+        @Nested
+        class PasswordStrengtInvalid{
+            @Test
+            @DisplayName("Prueba de contraseña invalida")
+            void InvalidoShouldReturnInvalid(){
+                //Arange
+                String password = null;
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.INVALID;
+                //ACT
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+
+                //Assert
+                assertEquals(actual, expected);
+            }
+            @Test
+            @DisplayName("Prueba de contraseña error")
+            void InvalidoShouldReturnERror(){
+                //Arange
+                String password = "123abc_";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.INVALID;
+                //ACT
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+
+                //Assert
+                assertEquals(actual, expected);
+            }
+            @Test
+            @DisplayName("Prueba de contraseña vacia")
+            void InvalidoShouldReturnvacio(){
+                //Arange
+                String password = "";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.INVALID;
+                //ACT
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+
+                //Assert
+                assertEquals(actual, expected);
+            }
+        }
     }
