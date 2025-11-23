c6adb5358bb15e316ea08f69fef53835b52d94e0
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-24 00:14:34 +0100

    test medium

--- src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
@@ -1,3 +1,46 @@
 package ud4.methods;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
     class PasswordStrenghtCheckerTest {
+        @Nested
+        class PasswordStrengtMedium {
+
+
+            @Test
+            @DisplayName("Prueba de contraseña medio minuscula mayuscula")
+            void MediumShouldReturnMedioMinMay() {
+                //Arrange
+                String password ="abcdEFGH";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.MEDIUM;
+                //Act
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+            @Test
+            @DisplayName("Prueba de contraseña medio minuscula simbolo")
+            void MediumShouldReturnMedioMinSimb() {
+                //Act
+                String password ="abcd!·$%";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.MEDIUM;
+                //Arange
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+            @Test
+            @DisplayName("Prueba de contraseña medio caracter mayuscula")
+            void MediumShouldReturnMedioMaySimb() {
+                //Act
+                String password ="ABCD&/()";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.MEDIUM;
+                //Arange
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+        }
     }
