2d7279f2575c5374343a25751929d8b918367fd2
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-24 00:16:07 +0100

    test strong

--- src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
+++ src/test/java/ud4/methods/PasswordStrenghtCheckerTest.java
@@ -1,3 +1,26 @@
 package ud4.methods;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
     class PasswordStrenghtCheckerTest {
+        @Nested
+        class PasswordStrengtStrong {
+
+
+            @Test
+            @DisplayName("Prueba de contraseña fuerte de todas")
+            void StrongShouldReturnFuerteMinMaySimb   () {
+                //Arrange
+                String password ="abcdEF··";
+                PasswordStrenghtChecker.PasswordStrength expected = PasswordStrenghtChecker.PasswordStrength.STRONG;
+                //Act
+                PasswordStrenghtChecker.PasswordStrength actual = PasswordStrenghtChecker.checkStrength(password);
+                //Assert
+                assertEquals(actual, expected);
+            }
+
+        }
     }
