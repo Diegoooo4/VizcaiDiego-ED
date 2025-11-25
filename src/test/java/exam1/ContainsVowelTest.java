33123ced64b9fb10ffe29aedf06ae02bcdf6eb9e
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-25 13:11:40 +0100

    Test sin vocales

--- src/test/java/exam1/ContainsVowelTest.java
+++ src/test/java/exam1/ContainsVowelTest.java
@@ -5,6 +5,33 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@Nested
 class ContainsVowelTest {
+    @Test
+    @DisplayName("Text with out vowels mayus")
+    void ShouldRturnWithOutVowelsMays(){
+        //Arrange
+        String str = "HLL WRLD";
+        boolean expected = false;
+
+        //Act
+        boolean actual = ContainsVowel.containsVowel(str);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Text with out vowels minus")
+    void ShouldRturnWithOutVowels(){
+        //Arrange
+        String str = "Hll wrld";
+        boolean expected = false;
+
+        //Act
+        boolean actual = ContainsVowel.containsVowel(str);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
 }
+
