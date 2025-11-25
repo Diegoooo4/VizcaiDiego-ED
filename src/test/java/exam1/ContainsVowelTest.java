a40f644c0c52794f3f0ed969d89d01c566f98560
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-25 13:02:56 +0100

    Punto de patida

---
+++ src/test/java/exam1/ContainsVowelTest.java
@@ -0,0 +1,10 @@
+package exam1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainsVowelTest {
+}
