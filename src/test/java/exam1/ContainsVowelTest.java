73768ce180ca28b98453a578fcb3fb12b9c2cbff
Diego Vizcaí Sesma <dievizses@edu.gva.es>
2025-11-25 13:08:47 +0100

    Test with vowels

--- src/test/java/exam1/ContainsVowelTest.java
+++ src/test/java/exam1/ContainsVowelTest.java
@@ -6,5 +6,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Nested
 class ContainsVowelTest {
+    @Test
+    @DisplayName("Text with vowels minus")
+    void ShouldRturnWithVowels() {
+        //Arrange
+        String str = "Hello world";
+        boolean expected = true;
+
+        //Act
+        boolean actual = ContainsVowel.containsVowel(str);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Text with vowels mayus")
+    void ShouldRturnWithVowelsMays(){
+        //Arrange
+        String str = "HELLO WORLD";
+        boolean expected = true;
+
+        //Act
+        boolean actual = ContainsVowel.containsVowel(str);
+        //Assert
+        assertEquals(expected, actual);
+    }
 }
